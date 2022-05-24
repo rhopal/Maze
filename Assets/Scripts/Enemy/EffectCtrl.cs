8654d7cfbfa59a4ee89ae578d6db54d19451ba88
rhopal <101325143+rhopal@users.noreply.github.com>
2022-05-24 21:44:57 +0900

    Threat Effect
    플레이어에게 적의 시야에 들어왔다고 알려주는 이펙트 제어

--- Assets/Scripts/Enemy/EffectCtrl.cs
+++ Assets/Scripts/Enemy/EffectCtrl.cs
@@ -6,25 +6,25 @@ using UnityEngine.UI;
 public class EffectCtrl : MonoBehaviour
 {
     public Image image;
-	private EnemyCtrl enemy;
+    private EnemyCtrl enemy;
     public Exit exit;
     private float alpha;
 
     void Start()
     {
-		enemy = GetComponent<EnemyCtrl>();
+	enemy = GetComponent<EnemyCtrl>();
     }
 
     void Update()
     {
-        if (exit.getAllKey == true)
+        if (exit.getAllKey == true) // 탈출구에 열쇠를 다 끼웠을 경우 이펙트 제거 후 정지
         {
             alpha = 0;
             image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
             return;
-        } // 탈출구에 열쇠를 다 끼우면 이펙트 제거하고 return
+        }
 
-        if (enemy.state != EnemyCtrl.State.PATROL) // Enemy가 플레이어를 바라보고 있지 않으면 이펙트를 점점 연하게
+        if (enemy.state != EnemyCtrl.State.PATROL) // 적이 플레이어를 바라보고 있지 않을 경우 이펙트 점점 연하게
         {
             alpha += 0.3f * Time.deltaTime;
             if (alpha >= 0.6f)
@@ -33,7 +33,7 @@ public class EffectCtrl : MonoBehaviour
             }
             image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
         }
-        else // Enemy가 플레이어를 바라보고 있으면 이펙트를 점점 진하게
+        else // 적이 플레이어를 바라보고 있으면 이펙트 점점 진하게
         {
             alpha -= 0.3f * Time.deltaTime;
             if (alpha <= 0)
